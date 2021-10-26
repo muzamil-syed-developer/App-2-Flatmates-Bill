2ededb74bcf8d448a308cfefa3d586574a8e7800
Muzamil@Work <Muzamil_Work@Muzs-MacBook-Air.local>
2021-10-26 16:28:25 -0700

    added implementation that generates a pdf

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+import webbrowser
+
 from fpdf import FPDF
 
 class Bill:
@@ -32,12 +35,39 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
 
+        #create pdf and page
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
 
+        #add image to the pdf
+        pdf.image("house.png", w=30, h=30)
+
+        #add title with appropriate font
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
+
+        #change font for text body
+        pdf.set_font(family="Times", size=12)
+
+        #add body text
+        pdf.cell(w=100, h=40, txt="Period:", border=1)
+        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
+        pdf.cell(w=100, h=40, txt=str("{:.2f}".format(flatmate1.pays(the_bill,flatmate2))), border=1, ln=1)
+        pdf.cell(w=100, h=40, txt=flatmate2.name, border=1)
+        pdf.cell(w=100, h=40, txt=str("{:.2f}".format(flatmate2.pays(the_bill, flatmate1))), border=1, ln=1)
+
+        #output file
+        pdf.output(self.filename)
+
+#test code
 the_bill = Bill(amount = 120, period = "April 2021")
 john = Flatmate(name = "John", days_in_house = 20)
 marry = Flatmate(name = "Marry", days_in_house = 25)
 
 print("John pays: ", john.pays(the_bill,flatmate2=marry))
 print("Marry pays: ", marry.pays(the_bill,flatmate2=marry))
+
+pdf_report = PdfReport(filename="Report1.pdf")
+pdf_report.generate(flatmate1=john,flatmate2=marry, bill=the_bill)
