37c4e20d1e63ae2c2c0c419de155d27bbb730a12
Muzamil Syed <Muzamil_Work@Muzs-MacBook-Air.local>
2021-10-26 17:44:21 -0700

    stabilize

--- reports.py
+++ reports.py
@@ -1,9 +1,6 @@
 import os
 import webbrowser
-
 from fpdf import FPDF
-
-from main import the_bill
 
 
 class PdfReport:
@@ -13,31 +10,30 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-
-        #create pdf and page
+        # create pdf and page
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
 
-        #add image to the pdf
+        # add image to the pdf
         pdf.image("house.png", w=30, h=30)
 
-        #add title with appropriate font
+        # add title with appropriate font
         pdf.set_font(family='Times', size=24, style='B')
         pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
 
-        #change font for text body
+        # change font for text body
         pdf.set_font(family="Times", size=12)
 
-        #add body text
+        # add body text
         pdf.cell(w=100, h=40, txt="Period:", border=1)
         pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
         pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
-        pdf.cell(w=100, h=40, txt=str("{:.2f}".format(flatmate1.pays(the_bill,flatmate2))), border=1, ln=1)
+        pdf.cell(w=100, h=40, txt=str("{:.2f}".format(flatmate1.pays(bill, flatmate2))), border=1, ln=1)
         pdf.cell(w=100, h=40, txt=flatmate2.name, border=1)
-        pdf.cell(w=100, h=40, txt=str("{:.2f}".format(flatmate2.pays(the_bill, flatmate1))), border=1, ln=1)
+        pdf.cell(w=100, h=40, txt=str("{:.2f}".format(flatmate2.pays(bill, flatmate1))), border=1, ln=1)
 
-        #output file
+        # output file
         pdf.output(self.filename)
 
-        #open pdf
-        webbrowser.open("file://"+os.path.realpath(self.filename))
+        # open pdf
+        webbrowser.open("file://" + os.path.realpath(self.filename))
