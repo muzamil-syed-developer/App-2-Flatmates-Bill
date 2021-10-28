db5f737d85117cd738662965945310385bef72e6
Muzamil Syed <Muzamil_Work@Muzs-MacBook-Air.local>
2021-10-27 17:38:06 -0700

    convert main script to command line interface

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from flat import Bill, Flatmate
 from reports import PdfReport
 
-#test code
-the_bill = Bill(amount = 120, period ="April 2021")
-john = Flatmate(name ="John", days_in_house = 20)
-marry = Flatmate(name ="Marry", days_in_house = 25)
+period = input("Enter the period for the bill: e.g January 2021 ")
+amount = float(input("Enter the bill amount: "))
 
-print("John pays: ", john.pays(the_bill,flatmate2=marry))
-print("Marry pays: ", marry.pays(the_bill,flatmate2=marry))
+f1 = input("Enter the name of the first flatmate: ")
+d1 = int(input(f"Enter the amount of days {f1} stayed in the house: "))
+
+f2 = input("Enter the name of the second flatmate: ")
+d2 = int(input(f"Enter the amount of days {f2} stayed in the house: "))
+
+the_bill = Bill(amount=amount, period=period)
+mate1 = Flatmate(name=f1, days_in_house=d1)
+mate2 = Flatmate(name=f2, days_in_house=d2)
 
 pdf_report = PdfReport(filename="Report1.pdf")
-pdf_report.generate(flatmate1=john,flatmate2=marry, bill=the_bill)
+pdf_report.generate(flatmate1=mate1, flatmate2=mate2, bill=the_bill)
